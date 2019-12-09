fa5ea7f1455bf7d7fbba91a00ba263c76b4d6894
施荣 <1309873659@qq.com>
2019-12-09 11:21:37 +0800

    update default shard to 0

--- static/js/config.js
+++ static/js/config.js
@@ -9,17 +9,17 @@ export default {
     nodes:[
       {
         name:'Harmony',
-        url:'https://api.s1.t.hmny.io/',
+        url:'https://api.s0.t.hmny.io/',
         chainId: 1
       },
       {
         name:'Pangaea',
-        url:'https://api.s1.pga.hmny.io/',
+        url:'https://api.s0.pga.hmny.io/',
         chainId: 3
       },
       {
         name:'Harmony Testnet',
-        url:'https://api.s1.b.hmny.io/',
+        url:'https://api.s0.b.hmny.io/',
         chainId: 2
       },
       {
